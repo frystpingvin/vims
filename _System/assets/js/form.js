58766594aaa0d18c98b3fdd70638fbbbf8c78625
Samuel Lie <samuel.lie1@gmail.com>
2016-07-18 14:44:05 +0200

    Update forms, add workpage and change personal

--- _System/assets/js/form.js
+++ _System/assets/js/form.js
@@ -1,8 +1,32 @@
 $.validate({
-  form : '#registrationForm'
+  form : '#registrationForm, #feedbackForm, #jobForm'
 });
 
-$('#registrationForm').ajaxForm(function() { 
-  $('#message').addClass('success').fadeIn().html('<h3>Tack för din köanmälan, vänligen spara bekräftelsemailet som du mottagit på e-post.</h3>'); 
-  $('#registrationForm').fadeOut();//Hide the form 
+// Registration form
+$('#registrationForm').ajaxForm(function() {
+  $('#message').addClass('success').fadeIn().html('<h3>Tack för din köanmälan, vänligen spara bekräftelsemailet som du mottagit på e-post.</h3>');
+  $('#registrationForm').fadeOut();//Hide the form
+});
+
+// Feedback form
+$('#feedbackForm').ajaxForm(function() {
+  $('#message').addClass('success').fadeIn().html('<h3>Tack för din feedback. Vi återkommer inom kort.</h3>');
+  $('#feedbackForm').fadeOut();//Hide the form
+});
+
+var concerning = jQuery('.concerning');
+var select = this.value;
+concerning.change(function () {
+  console.log('change');
+
+  if ($(this).val() == 'Annan') {
+      $('#otherType').show();
+  }
+  else $('#otherType').hide();
+});
+
+// Job form
+$('#jobForm').ajaxForm(function() {
+  $('#message').addClass('success').fadeIn().html('<h3>Tack för din intresseanmälan, vi hör av oss om vi hittar en lämplig anställning för dig.</h3>');
+  $('#jobForm').fadeOut();//Hide the form
 });
